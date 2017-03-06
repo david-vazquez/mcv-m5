7c31477164d3071ea9d5b14c237baa408d119fb6
lluisgomez <lgomez@cvc.uab.es>
2017-03-06 11:52:10 +0100

    Bug fix. Create the correct optimizer when cf.optimizer is 'sgd'

--- code/tools/optimizer_factory.py
+++ code/tools/optimizer_factory.py
@@ -15,7 +15,7 @@ class Optimizer_Factory():
         elif cf.optimizer == 'adam':
             opt = Adam(lr=1E-3, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
 
-        elif cf.optimizer == 'adam':
+        elif cf.optimizer == 'sgd':
             opt = SGD(lr=1E-3, momentum=0.9, nesterov=True)
 
         else:
